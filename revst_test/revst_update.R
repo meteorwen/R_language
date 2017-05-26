d5ad31d2c12f4e947348a6741aeb7d94166912c9
Jelly <meteor.wen@gmail.com>
2017-05-26 12:01:19 +0800

    update setTxtProgressBar

--- revst_test/revst_update.R
+++ revst_test/revst_update.R
@@ -23,13 +23,6 @@ t1 %>%
   gsub('\t|\r|\n', '',.)
 
 
-http://ziweidongjin.fang.com/?ctm=1.xian.xf_search.lplist.4
-http://jinseyuechengwk.fang.com/?ctm=1.xian.xf_search.lplist.3
-http://hailunchuntian029.fang.com/?ctm=1.xian.xf_search.lplist.8
-http://shanghejun029.fang.com/?ctm=1.xian.xf_search.lplist.10
-
-
-
 ---------------------------------------------------------------------------------------
 # 处理单网页、多标签
 nodes <- c('div.nlcd_name',
@@ -99,6 +92,137 @@ urls_Parse <- function(urls, nodes, time_limit){
 }
 
 test3 <-  urls_Parse(urls, nodes,time_limit = 2)
+
+
+
+
+
+--------------------------------------------------------------------------------------
+#抓取所房产地址链接，约合650个房产源
+--------------------------------------------------------------------------------------
+library(rvest)
+library(R.utils)
+library(stringr)
+
+url <- 'http://newhouse.xian.fang.com/house/s/b9'
+urls <- paste0(url,c(12,19,25))
+
+fun <- function(x){
+  return(x %>% 
+           read_html(encoding = 'gbk') %>% 
+           html_nodes('div.clearfix div.nlc_img a') %>%  
+           html_attr("href"))
+}
+
+pb <- txtProgressBar(max = length(urls), style = 3)  
+result <- vector()
+for(i in 1:length(urls)){
+  tryCatch({
+    evalWithTimeout(try(result <- c(result,fun(urls[i])), 
+                        silent = TRUE), timeout = 2)
+  }, error = function(e){
+    result <- c(result,e)
+  })
+  setTxtProgressBar(pb,  i)
+  gc()
+  closeAllConnections()
+}
+a #获取所有房产所有地址
+close(pb)
+
+write.csv(a,"fang.csv")
+--------------------------------------------------------------------------------------------------
+#提取各个链接的地址内的详细信息 test 查询标签测试
+--------------------------------------------------------------------------------------------------
+uu <- 'http://jingyinghuiqj.fang.com/'
+c('div.fixnav_tit h2',                #name
+  'div.tit a',                        #score
+  'div.advice_left',                 #phone
+  'div.wai p',                        #dynamic 
+  'div.information_li div.inf_left')  #detail
+  uu %>% 
+read_html(encoding = 'gbk') -> t
+t %>% 
+  html_nodes('div.advice_left') %>%   #指定标签
+  html_text() %>% 
+  gsub('\t|\r|\n', '',.) %>% 
+  paste(collapse=";")   #向量字符串合并为一个
+
+
+
+nodes <- c('div.fixnav_tit h2',                #name
+           'div.tit a',                        #score
+           'div.advice_left',                 #phone
+           'div.wai p',                        #dynamic 
+           'div.information_li div.inf_left')  #detail
+
+
+
+url <- 'http://jingyinghuiqj.fang.com/'
+-------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------
+#单网页，多标签自定义函数
+url_Parse <- function(url,nodes){
+  parsed_url <- read_html(url, encoding = 'gbk')   #读取单网页，常量
+  temp  <- lapply(nodes, function(node){
+    parsed_url %>% 
+      html_nodes(node) %>% 
+      html_text() %>% 
+      gsub('\U00A0|\U2002|\t|\r|\n|', '',.) %>% 
+      paste(collapse=";") 
+  })
+
+  result <- data.frame(name = temp[[1]],
+                       score = temp[[2]],
+                       phone = temp[[3]],
+                       dynamic = temp[[4]],
+                       detail = temp[[5]],
+                       stringsAsFactors=FALSE)
+  return(result)
+}
+
+#多网页多标签自定义函数
+urls_Parse <- function(urls, nodes, time_limit){
+  result <- matrix(NA, nrow = length(urls), ncol = 1 + length(nodes), 
+                   dimnames = list(urls, c('id','name','score','phone','dynamic','detail'))) %>% 
+    as.data.frame()  #分配内存指定
+  
+  pb <- txtProgressBar(max = length(urls), style = 3)   #显示进度
+  
+  for (url in urls) {
+    tryCatch({
+      evalWithTimeout(try(result[url, ] <- url_Parse(url, nodes), 
+                          silent = TRUE), timeout = time_limit)
+    }, error = function(e){
+      result[url, ] <- e
+    })
+    
+    setTxtProgressBar(pb, which(urls == url))
+    gc()
+    closeAllConnections()
+  }
+  
+  close(pb)
+  return(result)
+}
+-------------------------------------------------------------------------------------------------
+test <-  urls_Parse(urls, nodes,time_limit = 0.5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
